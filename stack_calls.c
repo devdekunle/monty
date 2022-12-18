3e40e3f6caee0d642f63662529a89925f2d6eeb1
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2022-12-18 13:28:13 -0800

    fixed pop

--- stack_calls.c
+++ stack_calls.c
@@ -36,6 +36,7 @@ void pop_stack(stack_t **stack, unsigned int line_number)
 	if (len_dlistint(*stack) == 0)
 	{
 		dprintf(2, "L%u: can't pop an empty stack\n", line_number);
+		printf("1\n");
 		free_list(*stack);
 		free(global_t.ptr_line);
 		exit(EXIT_FAILURE);
